c492cac731a26482ea2e5af413981b42df291322
Andy1iang <andyliang254@gmail.com>
2024-11-10 14:07:35 -0500

    Redid day 8

---
+++ Day08/day08_p1.py
@@ -0,0 +1,30 @@
+FILE_NAME = 'day08.txt'
+
+lines = open(FILE_NAME).readlines()
+for i in range(len(lines)):
+    lines[i] = lines[i].strip()
+
+directions = lines[0]
+
+# Create a dictionary of nodes
+# key: node name; value: tuple of left and right nodes
+nodes = {}
+for line in lines[2::]:
+    nodes[line[0:3:]] = (line[7:10:], line[12:15:])
+
+total = 0
+curr = 'AAA'
+end = 'ZZZ'
+
+# traverse while we are not at ZZZ node
+while curr != end:
+    # if the direction is left, go to the left node, else go to the right node
+    if directions[total%len(directions)] == 'L':
+        curr = nodes[curr][0]
+    else:
+        curr = nodes[curr][1]
+
+    total += 1
+
+print(total)
+    
---
+++ Day08/day08_p2.py
@@ -0,0 +1,39 @@
+from math import lcm
+# brute force takes too long
+# so we need to find the least common multiple of the cycle lengths of each node
+# lcm is when they will reach Z node at the same time
+
+FILE_NAME = 'day08.txt'
+
+lines = open(FILE_NAME).readlines()
+for i in range(len(lines)):
+    lines[i] = lines[i].strip()
+
+directions = lines[0]
+
+nodes = {}
+for line in lines[2::]:
+    nodes[line[0:3:]] = (line[7:10:], line[12:15:])
+
+# function to get the length of path from A to Z for a given node
+def getPath(node):
+    cnt = 0
+    while node[2] != 'Z':
+        if directions[cnt%len(directions)] == 'L':
+            node = nodes[node][0]
+        else:
+            node = nodes[node][1]
+        cnt += 1
+    
+    return cnt
+
+# count of all the path
+pathCounts = []
+for node in nodes:
+    if node[2] == 'A':
+        pathCounts.append(getPath(node))
+
+# returning the lcm
+# *pathCounts is used to unpack the list
+print(lcm(*pathCounts))
+    
